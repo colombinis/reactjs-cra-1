087e81c5ad858c2c6628f066075b0c376e137bed
Colombini <colombinis@gmail.com>
2019-01-30 10:20:01 -0300

    Shows list of Personas from component Personas passing list throught props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Personas from './components/Personas';
 
 
 class App extends Component {
+  state = {
+    personas:[
+      {id:1, name:"Sebastian", edad:"42", profesion:"dev"},
+      {id:2, name:"Ale", edad:"33", profesion:"qa"},
+      {id:3, name:"Colo", edad:"22", profesion:"dev"}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Starting from CRA</h1>
          
-        <Personas />
+        <Personas personas={this.state.personas}  />
+        
         
       </div>
     );
--- src/components/Personas.js
+++ src/components/Personas.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 
 
 class Personas extends Component {
+
+  
   render() {
+    const {personas } = this.props;
+
+    const PersonaList = personas.map( p => {
+      return (
+        <div key={p.id} className="Personas">
+          <h2>Componente Persona</h2>
+          <div>Name : {p.name}</div>
+          <div>Edad : {p.edad}</div>
+          <div>Profesion : {p.profesion}</div>           
+        </div>
+      )
+    });
+
     return (
-      <div className="Persona">
-      <h2>Componente Persona</h2>
-         <div>Name:</div>
-         <div>Edad:</div>
-         <div>Profesion:</div>
-          
+      <div className="personas-list">
+        {PersonaList}          
       </div>
     );
   }
