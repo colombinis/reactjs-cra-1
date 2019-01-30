15f5ec8f52483ac28a2ac51c4a7a8025adbfa183
Colombini <colombinis@gmail.com>
2019-01-30 09:53:25 -0300

    Remove assets and create new Persona component

---
+++ src/components/Personas.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Personas extends Component {
+  render() {
+    return (
+      <div className="Persona">
+      <h2>Componente Persona</h2>
+         <div>Name:</div>
+         <div>Edad:</div>
+         <div>Profesion:</div>
+          
+      </div>
+    );
+  }
+}
+
+export default Personas;
